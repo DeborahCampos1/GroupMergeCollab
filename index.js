14e93773cc33938e53558714bc25ed78ad4aa1cc
CarlosAMoran <Carlosamoran18@gmail.com>
2021-12-02 15:50:59 -0500

    added banana function

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ function fn(...c) {
   
   fn(1, 2, 3); //> 6
   fn(10, "B", 20); //> error All arguments must be numbers.
+
+  function banana() {
+    this.val1 = "value1";
+    this.val2 = "value2";
+    let newBananaValue = this.val1;
+    this.bananaMethod = function () {
+        myBananaValue = newBananaValue;
+        return myBananaValue;
+    };
+}
+let bananaInstance = new banana();
+alert(bananaInstance.bananaMethod());
   
